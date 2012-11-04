9f5b7b177847002cbadab59de9369087ec9b58a7
Jesse Bounds <jesse@rebounds.net>
2012-11-04 10:11:42 -0800

    tile extraction working

--- brain_train.js
+++ brain_train.js
@@ -1,17 +1,59 @@
 var Canvas = require('canvas')
-  , canvas = new Canvas(200,200)
   , Image = Canvas.Image
-  , ctx = canvas.getContext('2d')
   , fs = require('fs');
 
-fs.readFile(__dirname + '/images/training_001.jpg', function(err, imgSrc){
-  if (err) throw err;
-  img = new Image;
-  img.src = imgSrc;
-  ctx.drawImage(img, 0, 0, img.width / 4, img.height / 4);
-  console.log(ctx);
-  _getPixelData();
-});
+var img = new Image;
+
+img.onerror = function(e){
+  throw e;
+};
+
+img.onload = function() {
+  var topY = 496 //XXX: 496 is for iphone5 screenshot
+    , topX = 0
+    , tileSize = 128
+    , row = 1
+    , col = 1;
+
+  function _extractAndWriteTile(){
+    var canvas = new Canvas(128, 128)
+      , ctx = canvas.getContext('2d')
+      , out = fs.createWriteStream(__dirname + '/images/tile_' + row + '_' + col + '.jpg');
+
+    ctx.drawImage(img, topX, topY, 128, 128, 0, 0, 128, 128);
+
+    var stream = canvas.createJPEGStream({
+      bufsize : 2048,
+      quality : 80
+    });
+
+    stream.on('end', function(d){
+      _stage();
+    });
+
+    stream.pipe(out);
+  };
+
+  function _stage(){
+    col = (col + 1 > 5) ? 0 : col + 1;
+
+    if (col > 0) {
+      topX += tileSize;
+    } else {
+      topY += tileSize;
+      topX = 0;
+      col = 1;
+      row += 1;
+    }
+
+    if (row + 1 > 6) { return; } // done
+    _extractAndWriteTile();
+  }
+
+  _extractAndWriteTile();
+};
+
+img.src = __dirname + '/images/training_001.jpg';
 
 function _getPixelData() {
   var data = ctx.getImageData(0, 0, 1, 1);
