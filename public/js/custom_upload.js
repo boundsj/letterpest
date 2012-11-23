939bd61302b1acae59af7ba0be3d8130362d127f
Jesse Bounds <jesse@rebounds.net>
2012-11-23 10:56:48 -0800

    commenting loading spinner for now

--- public/js/custom_upload.js
+++ public/js/custom_upload.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
     $('#upload-real').click();
   });
   $('#upload-real').bind("change", function(){
-    console.log("file picked :/");
-    $('.title_main').hide();
-    $('.mouse_main').hide();
-    $('.inst_main').hide();
-    $('#upload').replaceWith($('.spinner').show());
+    //XXX commenting spinner for now :(
+    //$('.title_main').hide();
+    //$('.mouse_main').hide();
+    //$('.inst_main').hide();
+    //$('#upload').replaceWith($('.spinner').show());
     $('#submit-file').click();
   });
 });
